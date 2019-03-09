32f46d607ebdd3ba06bf32ff7e010c6c0834dcec
Deepak Maheshwari <maheshwari.deepak@gmail.com>
2019-03-10 06:34:08 +0800

    Add files via upload

---
+++ Scripts/AzureLocks-Implement-locks-on-AlltheResourceGroups-in-a-Subscription.ps1
@@ -0,0 +1,29 @@
+﻿##This script helps to create and delete a resource lock on a resource group
+
+Login-AzureRmAccount
+
+select-azurermsubscription -SubscriptionID "Id of your subscription" 
+
+## View the locks applied on a subscription
+Get-AzureRmResourceLock | ft
+
+##How to lock all the resource groups in a subscription 
+
+$RGName = Get-AzureRmResourceGroup
+
+    foreach($RG in $RGName)
+
+    {
+       New-AzureRMResourceLock -LockName "LockRG" -LockLevel CanNotDelete -ResourceGroupName $RG.ResourceGroupName -Force
+    }
+
+
+
+##How to unlock all the resource groups in a subscription 
+
+$RGName = Get-AzureRmResourceGroup
+foreach($RG in $RGName)
+
+    {
+       remove-AzureRMResourceLock -LockName "LockRG" -ResourceGroupName $RG.ResourceGroupName -Force
+    }
