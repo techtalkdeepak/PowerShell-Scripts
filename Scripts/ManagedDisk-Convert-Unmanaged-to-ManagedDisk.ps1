afc09fcbe89b03f30e30fdacc14c57bd240383a8
Deepak Maheshwari <maheshwari.deepak@gmail.com>
2019-03-10 07:06:10 +0800

    Add files via upload

---
+++ Scripts/ManagedDisk-Convert-Unmanaged-to-ManagedDisk.ps1
@@ -0,0 +1,12 @@
+﻿$VMName = "VM Name"
+$RGName = "Resource Group Name"
+
+## Login to Azure RM Account
+Login-Azurermaccount
+
+## Select an Azure Subscription 
+Select-AzureRMSubscription -Subscriptionname "Your Subscription Name"
+
+##Converting an unmanaged VM disk to Managed 
+
+ConvertTo-AzureRmVMManagedDisk -ResourceGroupName $RGName -vmname $VMName
