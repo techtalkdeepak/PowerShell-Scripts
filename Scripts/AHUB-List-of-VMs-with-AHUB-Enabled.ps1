c3b096f4fdf28b4fa3706d505430465093702b09
Deepak Maheshwari <maheshwari.deepak@gmail.com>
2019-03-10 07:53:29 +0800

    Add files via upload

---
+++ Scripts/AHUB-List-of-VMs-with-AHUB-Enabled.ps1
@@ -0,0 +1,19 @@
+﻿##See a list of VMs with Azure Hybrid Use Benefits enabled 
+
+Login-AzureRmAccount
+
+select-azurermsubscription -SubscriptionID "Id of your subscription" 
+
+##List of VMs with all the license Types 
+Get-azurermvm| select ResourceGroupName, Name, LicenseType
+
+##List of VMs with Azure Hybrid Use Benefit license type "Windows_Server"
+
+Get-azurermvm| ?{$_.LicenseType -like "Windows_Server"} | select ResourceGroupName, Name, LicenseType
+
+##List of VMs with No Azure Hybrid Use Benefit license type "Windows_Server"
+
+Get-azurermvm| ?{$_.LicenseType -notlike "Windows_Server"} | select ResourceGroupName, Name, LicenseType
+
+
+
