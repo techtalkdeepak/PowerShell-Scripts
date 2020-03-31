35ac87fa761436e3867f6cc096ff940ea941699f
Deepak Maheshwari <maheshwari.deepak@gmail.com>
2020-03-31 21:21:06 +0800

    Add files via upload

---
+++ Scripts/EmptyNSG.ps1
@@ -0,0 +1,31 @@
+﻿<#
+Description: List of NSGs with empty security rules
+	Type:			Report
+	Report:			Empty NSG's
+	Output:			Console
+	Subscription:	All Subscriptions
+#>
+
+#Login to Azure
+Login-AzAccount > $null
+
+#Get all subscriptions
+$subscriptions = (get-azsubscription).ID
+
+#Define common variables
+$emptynsg = @()
+
+#Loop through all subscriptions
+foreach ($sub in $subscriptions)
+{
+	#Change the subscription context
+	Select-AzSubscription -SubscriptionId $sub >> NULL
+	
+	foreach ($nsg in (get-aznetworksecuritygroup))
+    {
+        if (!($nsg.SecurityRules))
+        {
+           $emptynsg += $nsg.Name
+        }
+    }
+} 
